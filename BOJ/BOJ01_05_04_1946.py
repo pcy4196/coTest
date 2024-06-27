47b67bad85adf8ffd597ae11ce6a3e54f7d87417
Park, Chan-Yu <pcy4196@kakao.com>
2024-06-28 00:07:20 +0900

    [BOJ](백준) - 1946 문제 수정

--- BOJ/BOJ01_05_04_1946.py
+++ BOJ/BOJ01_05_04_1946.py
@@ -3,22 +3,25 @@ import sys
 input = sys.stdin.readline
 
 ans = []
-T = int(input())
+T = int(input())        # 테스트 케이스의 수를 입력 받음
 for _ in range(T):
-    N = int(input())
+    N = int(input())    # 각 테스트 케이스의 지원자 수를 입력 받음
     arr = []
     for _ in range(N):
+        # 각 지원자의 서류 심사 순위와 면접 순위를 입력 받아 리스트에 추가
         arr.append(list(map(int, input().split())))
-    arr.sort()
-    cnt = 1
-    rank = arr[0][1]
-    for i in range(1, N):
-        vsNum = arr[i][1]
-        if vsNum < rank:
-            cnt += 1
-        rank = min(vsNum, rank)
-    
-    ans.append(cnt)
+    arr.sort()  # 서류 심사 순위를 기준으로 오름차순 정렬
+
+    cnt = 1             # 첫 번째 지원자는 무조건 선발
+    rank = arr[0][1]    # 첫 번째 지원자의 면접 순위를 저장
+
+    for i in range(1, N):   # 두 번째 지원자부터 반복
+        vsNum = arr[i][1]   # 현재 지원자의 면접 순위
+        if vsNum < rank:    # 현재 지원자의 면접 순위가 기존의 최소 면접 순위보다 낮으면
+            cnt += 1        # 선발 인원 증가
+            rank = vsNum    # 최소 면접 순위를 현재 지원자의 면접 순위로 갱신
+
+    ans.append(cnt)         # 각 테스트 케이스의 결과를 리스트에 추가
 
 for i in range(T):
-    print(ans[i])
+    print(ans[i])  # 각 테스트 케이스의 결과 출력
