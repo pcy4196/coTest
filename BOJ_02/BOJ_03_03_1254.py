a512d892e52869a698466b7b6b91a201a48ad0ac
Park, Chan-Yu <pcy4196@kakao.com>
2024-09-21 22:39:29 +0900

    [BOJ](백준)_Season2 - 1254 문제 정답코드에 주석 추가

--- BOJ_02/BOJ_03_03_1254.py
+++ BOJ_02/BOJ_03_03_1254.py
@@ -4,12 +4,17 @@ input = sys.stdin.readline
 
 S = input().rstrip()
 
+# 문자열 S의 각 부분 문자열을 순차적으로 확인
 for i in range(len(S)):
-    p = S[i:]
-    rP = ''
+    p = S[i:]   # S의 i번째 인덱스부터 끝까지의 부분 문자열을 p에 저장
+    rP = ''     # p의 역순 문자열을 저장할 변수
+    
+    # 부분 문자열 p를 역순으로 만들어 rP에 저장
     for j in p:
         rP = j + rP
+    
+    # 부분 문자열 p가 그 역순인 rP와 같다면 펠린드롬 
+    # 아닌 부분의 개수만 추가하여 전체 문자를 펠드롬 처리
     if p == rP:
         print(len(S) + i)
         break
-    
